df3e4515812521d674566bf638a5c5378f535190
Minerva2 <3482752714@qq.com>
2019-04-23 15:59:22 +0800

    img-responsive
    bootstrap img-responsive

--- src/main/java/com/springboot/sample/schedule/ScheduleTask.java
+++ src/main/java/com/springboot/sample/schedule/ScheduleTask.java
@@ -19,24 +19,24 @@ public class ScheduleTask {
      * 定时任务
      * 配置上一次任务结束时间和下一次任务开始时间的间隔
      */
-    @Scheduled(fixedDelay = 3000)
-    public void delayTask(){
-        System.out.println("delay schedule task,time="+ LocalDateTime.now());
-    }
+//    @Scheduled(fixedDelay = 3000)
+//    public void delayTask(){
+//        System.out.println("delay schedule task,time="+ LocalDateTime.now());
+//    }
 
     /**
      * 配置上一次任务开始时间和下一次任务开始时间的间隔
      */
-    @Scheduled(fixedRate = 4000)
-    public void rateTask(){
-        System.out.println("rate schedule task,time="+ LocalDateTime.now());
-    }
+//    @Scheduled(fixedRate = 4000)
+//    public void rateTask(){
+//        System.out.println("rate schedule task,time="+ LocalDateTime.now());
+//    }
 
     /**
      * 配置上一次任务开始时间和下一次任务开始时间的间隔，如果到下一次任务开始时间，但是上一次任务未完成，则跳过本次执行
      */
-    @Scheduled(cron = "0/5 * * * * ?")
-    public void cronTask(){
-        System.out.println("cron schedule task,time="+ LocalDateTime.now());
-    }
+//    @Scheduled(cron = "0/5 * * * * ?")
+//    public void cronTask(){
+//        System.out.println("cron schedule task,time="+ LocalDateTime.now());
+//    }
 }
