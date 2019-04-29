61b8c08d1ee2d060b1693982116966f455adeb49
Minerva2 <3482752714@qq.com>
2019-04-29 13:59:26 +0800

    javadoc
    add javadoc

--- src/main/java/com/springboot/sample/controller/RestUserController.java
+++ src/main/java/com/springboot/sample/controller/RestUserController.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 /**
- * rest接口
+ * @Description rest接口
+ * @Author leiyi
  */
 @RestController
 public class RestUserController {
