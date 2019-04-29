6d0f7de23188abf62eba9825a858ded3bbf3afc5
Minerva2 <3482752714@qq.com>
2019-04-29 13:59:08 +0800

    RequestMapping
    the same problem

--- src/main/java/com/springboot/sample/controller/BootstrapController.java
+++ src/main/java/com/springboot/sample/controller/BootstrapController.java
@@ -1,13 +1,14 @@
 package com.springboot.sample.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/bootstrap")
 public class BootstrapController {
 
-    @RequestMapping("/demo")
+    @GetMapping("/demo")
     public String demo(){
         return "demo";
     }
